464b75c82c8ea11726b0a5533cd0def2beb8c7d1
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2015-04-09 16:59:16 -0700

    Update HW4.c

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -2,39 +2,73 @@
 int main()
 {
 //high nibble for output(columns) low for input(rows);
-DDRB=0xF0;
+DDRA = 0x10001011;
+//DDRA = 0x01110100;
+
 //enable internal pullups for PB0-PB3
-PORTB=0x0F;
-//Port D for indication only
-DDRD=0xFF;
+PORTA= 0x0F;
+
+//Port B for indication only
+DDRB=0xFF;
+//SFIOR = 0 << PUD;
+PORTB=0xff;
+
+unsigned Mask = DDRA;
+
 while (1) //loop key check forever
 	{
+
+
 		//first column
-		PORTB =0b01111111;
+		PORTA =0b11111011;
 		//check for rows and send key number to portD
 		//instead sending key number to PORTD you can use
 		// any function that serves pressed button
-		if (bit_is_set(PINB, 3)) PORTD=1;
-		if (bit_is_set(PINB, 2)) PORTD=2;
-		if (bit_is_set(PINB, 1)) PORTD=3;
-		if (bit_is_set(PINB, 0)) PORTD=4;
+		
+		// Not work
+		if (!bit_is_set(PINA, 7)) PORTB=0x01;
+		// 1
+		if (!bit_is_set(PINA, 3)) PORTB=0x02;
+		// Not work
+		if (!bit_is_set(PINA, 1)) PORTB=3;
+		// Not work
+		if (!bit_is_set(PINA, 0)) PORTB=4;
+
+
 		//second column
-		PORTB =0b10111111;
-		if (bit_is_set(PINB, 3)) PORTD=5;
-		if (bit_is_set(PINB, 2)) PORTD=6;
-		if (bit_is_set(PINB, 1)) PORTD=7;
-		if (bit_is_set(PINB, 0)) PORTD=8;
+		PORTA =0b11101111;
+		// D
+		if (!bit_is_set(PINA, 7)) PORTB=0x03;
+		// Not work
+		if (!bit_is_set(PINA, 3)) PORTB=0x04;
+		// C
+		if (!bit_is_set(PINA, 1)) PORTB=5;
+		// B
+		if (!bit_is_set(PINA, 0)) PORTB=6;
+/*
+
 		//third column
-		PORTB =0b11011111;
-		if (bit_is_set(PINB, 3)) PORTD=9;
-		if (bit_is_set(PINB, 2)) PORTD=10;
-		if (bit_is_set(PINB, 1)) PORTD=11;
-		if (bit_is_set(PINB, 0)) PORTD=12;
+		PORTA =0b00000000;
+		// #
+		if (!bit_is_set(PINA, 3)) PORTB=0x55;
+		// Not work
+		if (!bit_is_set(PINA, 2)) PORTB=0x55;
+		// 9 
+		if (!bit_is_set(PINA, 1)) PORTB=0x55;
+		// 6
+		if (!bit_is_set(PINA, 0)) PORTB=0x55;
+
+
 		//fourth column
-		PORTB =0b11101111;
-		if (bit_is_set(PINB, 3)) PORTD=13;
-		if (bit_is_set(PINB, 2)) PORTD=14;
-		if (bit_is_set(PINB, 1)) PORTD=15;
-		if (bit_is_set(PINB, 0)) PORTD=16;
+		PORTA =0b00000000;
+		// 0
+		if (!bit_is_set(PINA, 3)) PORTB=13;
+		// 8
+		if (!bit_is_set(PINA, 2)) PORTB=14;
+		// Not work
+		if (!bit_is_set(PINA, 1)) PORTB=15;
+		// 5
+		if (!bit_is_set(PINA, 0)) PORTB=16;
+*/
 	}
 }
