66dc4436a61eecfda797120cd41f7375ed442c99
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2015-04-09 17:14:12 -0700

    Update HW4.c

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -1,6 +1,8 @@
 #include <avr/io.h>
 int main()
 {
+unsigned int i, dly;
+
 //high nibble for output(columns) low for input(rows);
 DDRA = 0x10001011;
 //DDRA = 0x01110100;
@@ -15,25 +17,48 @@ PORTB=0xff;
 
 unsigned Mask = DDRA;
 
+void delay (unsigned int dly)
+{
+for(i = dly; i != 0; i--) ;
+}
+
 while (1) //loop key check forever
 	{
 
 
 		//first column
-		PORTA =0b11111011;
+		PORTA =0b10111111;
 		//check for rows and send key number to portD
 		//instead sending key number to PORTD you can use
 		// any function that serves pressed button
 		
 		// Not work
-		if (!bit_is_set(PINA, 7)) PORTB=0x01;
-		// 1
-		if (!bit_is_set(PINA, 3)) PORTB=0x02;
+		// 00000111
+		if (bit_is_set(PINA, 7)) {
+		PORTB=0x01;
+		delay(55000U);
+		PORTB = 0xff;
+		}
+		// 00000011
+		if (bit_is_set(PINA, 3)) {
+		PORTB=0x02;
+		delay(55000U);
+		PORTB = 0xff;
+		}
 		// Not work
-		if (!bit_is_set(PINA, 1)) PORTB=3;
+		if (bit_is_set(PINA, 2)) {
+		PORTB=0x03;
+		delay(65000U);
+		PORTB = 0xff;
+		}
 		// Not work
-		if (!bit_is_set(PINA, 0)) PORTB=4;
-
+		// 00000100
+		if (bit_is_set(PINA, 4)) {
+		PORTB=0x04;
+		delay(65000U);
+		PORTB = 0xff;
+		}
+/*
 
 		//second column
 		PORTA =0b11101111;
@@ -45,7 +70,7 @@ while (1) //loop key check forever
 		if (!bit_is_set(PINA, 1)) PORTB=5;
 		// B
 		if (!bit_is_set(PINA, 0)) PORTB=6;
-/*
+
 
 		//third column
 		PORTA =0b00000000;
