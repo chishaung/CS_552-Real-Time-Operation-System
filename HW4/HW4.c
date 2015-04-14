faf11b12923a18c6952b9d21c219cebe338308c6
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2015-04-14 16:08:18 -0700

    Update HW4.c

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -13,43 +13,75 @@
 
 int main()
 {	
-	uint8_t MASK;
+	uint8_t COL_MASK;
 	uint8_t TEST;
 	DDRB = 0xFF;
 	PORTB = 0xFF;
+	
 	// 1 2 3 4 5 6 7 8
 	// R R C R C C C R
 
 	// 8 7 6 5 4 3 2 1
 	// R C C C R C R R
-
+	
+	// Set Row as Input, Column as Output
 	DDRA =  0b10001011;
+	// Pullup
 	PORTA = 0b01110100;
+
+
 	while (1) {
+
+	    // If Row 2 Pressed
+	    TEST = 0b01000000;
 	    
-	    if (PINA == 0b00110100) {
-	        PORTB = 0x01;
+	    if (!(PINA & TEST)) {
+		PORTA = 0b01000000;
+		// Col
+		if (bit_is_set(PINA, 1)){
+	            PORTB = ~0x12;
+		    _delay_ms(2);
+		    PORTB = 0xFF;
+		}
+		
+
+		if (bit_is_set(PINA, 3)){
+	            PORTB = ~0x22;
+		    _delay_ms(2);
+		    PORTB = 0xFF;
+		}
+
+	    }
+PORTA = 0b01110100;
+	    
+	    // If Row 2 Pressed
+	    TEST = 0b00100000;
+	    
+	    if (!(PINA & TEST)) {
+
+	        PORTB = ~0x02;
+		_delay_ms(2);
+		PORTB = 0xFF;
+	    }
+	    
+	    // If Row 2 Pressed
+	    TEST = 0b00010000;
+	
+	    if (!(PINA & TEST)) {
+ 
+	        PORTB = ~0x04;
 		_delay_ms(2);
 		PORTB = 0xFF;
 	    }
 
-	    if (PINA == 0b01010100) {
-	        PORTB = 0x02;
+            // If Row 2 Pressed
+	    TEST = 0b00000100;
+	    
+	    if (!(PINA & TEST)) {
+
+	        PORTB = ~0x08;
 		_delay_ms(2);
 		PORTB = 0xFF;
 	    }
-
-	    if (PINA == 0b01100100) {
-	        PORTB = 0x03;
-		_delay_ms(2);
-		PORTB = 0xFF;
-	    }
-
-	    if (PINA == 0b01110000) {
-	        PORTB = 0x04;
-		_delay_ms(2);
-		PORTB = 0xFF;
-	    }
-
 	}
 }
