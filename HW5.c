46c4ee423e2a5b879ac1cedb7c7159418c62e2fe
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2015-04-24 15:26:08 -0700

    Update HW5.c

--- HW5.c
+++ HW5.c
@@ -1,45 +1,63 @@
-/* RTOS Assignment 4 - The 4x4 KeyPad driver
+/* RTOS Assignment 5 - LCD Dsiplay
  * Author: Chihsiang Wang
- * Description: If the user enter the right password, the LED will 'Dance' for you. If enter the wrong password, the LED will blink for warning.
- * Password: 83AC or 69BD or 120#
+ * Description: PORTA - Control. PORTD - Data. LCD will display "HELLO WORLD!"
  * Code Size: 3.82KB
  */
 
 #include <avr/io.h>
 #include <util/delay.h>
-#define Display
+
 #define F_CPU 16000000UL
 
+void ReadData(char ch) {
+    PORTA = 0x02;
+    _delay_ms(50);
+    PORTA = 0x03;
+    _delay_ms(50);
+    PORTD = ch;
+    _delay_ms(50);
+    PORTA = 0x02;
+    _delay_ms(50);
+
+}
+
 void WriteData(char ch) {
-	PORTD = ch;
-	PORTA = 0x02;
-	_delay_ms(1);
-	PORTA = 0x03;
-	_delay_ms(1);
-	PORTA = 0x02;
-	_delay_ms(100);
+    PORTD = 0x00;
+    _delay_ms(50);
+    PORTD = 0x01;
+    _delay_ms(50);
+    ReadData(ch);
+    PORTD = 0x00;
+    _delay_ms(50);
 }
 
 void Init() {
-	// PORTA -> control, PORTD -> Data
-	_delay_ms(80); // Wait until VDD reach 4.5V
-	PORTD = 0x0F;
-	PORTA = 0x30;
-	_delay_ms(5);
-	PORTA = 0x30;
-	_delay_ms(5);
-	PORTA = 0x30;
-	_delay_ms(5);
-	PORTA = 0x03;
-	PORTA = 0x02;
-	PORTA = 0x30;
-	PORTA = 0x30;
+    // PORTA -> control, PORTD -> Data
+    DDRA = 0xFF;
+    DDRD = 0xFF;
+
+    _delay_ms(80); // Wait until VDD reach 4.5V
+    PORTA = 0x38;	// Function Set
+    PORTA = 0x0F;	// Turn On Display
+    PORTA = 0x01;	// Clear Display
+    PORTA = 0x02;	// Return Home
 }
 
 int main()
 {
-	Init();
-	WriteData('A');
 
-	
+	Init();
+        PORTA = 0x00;
+	WriteData('H');
+	WriteData('E');
+	WriteData('L');
+	WriteData('L');
+	WriteData('O');
+	PORTA = 0x40;
+	WriteData('W');
+	WriteData('O');
+	WriteData('R');
+	WriteData('L');
+	WriteData('D');
+	WriteData('!');  	
 }
