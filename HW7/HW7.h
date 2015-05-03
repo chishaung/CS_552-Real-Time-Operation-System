1e4a5189c1f269f628978f5f8d6d90dff0674266
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2015-05-03 16:50:47 -0700

    Update HW7.h

--- HW7/HW7.h
+++ HW7/HW7.h
@@ -1,6 +1,8 @@
 #ifndef HW7_H
 #define HW7_H
 #define MAX_TASK 5
+#include <avr/io.h>
+#include <util/delay.h>
 
 
 typedef struct Task_Control {
@@ -9,7 +11,7 @@ typedef struct Task_Control {
 	int Task_ID;
 	int status;
 	int Priority;
-	} Task_Control;
+} Task_Control;
 
 typedef struct Task_Control * TCBptr;
 
@@ -21,33 +23,79 @@ TCBptr TaskList[MAX_TASK];
 
 
 void Task_1() {
+	uint8_t cnt;
 	while (1) {
-		printf("ok1\n");
+		for ( cnt=0; cnt <= 0x0f; cnt++ )
+		{
+			PORTB = ~cnt;
+			_delay_ms(500);
+
+		}
+		
 	}
 }
 
 void Task_2() {
+	uint8_t cnt;
 	while (1) {
-		printf("ok2\n");
+		for ( cnt=0x0f; cnt >= 0x00; cnt-- )
+		{
+			PORTB = ~cnt;
+			_delay_ms(500);
+		}
 	}
 }
 
 void Task_3() {
+	uint8_t cnt;
 	while (1) {
-		printf("ok3\n");
+
+		if (PORTB == 0x00)
+			PORTB = 0xff;
+		PORTB = PORTB << 1;
+		_delay_ms(500);
+
 	}
 }
 
 void Task_4() {
+	uint8_t cnt = 0x55;
+	PORTB = 0x00;
 	while (1) {
-		printf("ok4\n");
+		
+		PORTB = 0x55;
+		_delay_ms(500);
+		PORTB = 0xFF;
+		_delay_ms(500);
 	}
 }
 
 void UIKIdle() {
 	while (1) {
-		printf("IDLE\n");
+		PORTB = 0xFE;
+		_delay_ms(300);
+		PORTB = 0xFF;
+		_delay_ms(500);
+		if (TaskList[0]->status = 1)
+			break;
 	}
 }
+
+ISR(TIMER0_OVF_vect)    // gcc syntax
+
+{
+	//UIKDispatcher();
+	while (PINA != 0xFF) {
+		TaskList[0]->status = 1;
+		TaskList[1]->status = 0;
+		UIKDispatcher();
+	}
+	
+	while (PINA == 0xFF) {
+		TaskList[0]->status = 0;
+		TaskList[1]->status = 1;
+		UIKDispatcher();
+	}			
+} // end ISR
 
 #endif
