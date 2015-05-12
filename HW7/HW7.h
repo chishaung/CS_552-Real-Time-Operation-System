de703d293845a5759fd2239fb304bd88c07c48df
Chihsiang Wang <wang0162@vandals.uidaho.edu>
2015-05-11 21:54:55 -0700

    Update HW7.h

--- HW7/HW7.h
+++ HW7/HW7.h
@@ -6,6 +6,11 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+
+typedef struct _UIKSem {
+    uint8_t value;
+} UIKSem;
+
 typedef struct Task_Control {
 
 	void (*taskptr)();
@@ -13,12 +18,18 @@ typedef struct Task_Control {
 	int status;
 	int Priority;
 	uint8_t* stack_ptr;
+	int init;		// If already Init, set to 1.
+	UIKSem* Sem;
 
 } Task_Control;
 
 typedef struct Task_Control * TCBptr;
 
 
+
+
+
+int ticknum = 0;
 int Task_Numbers;
 int Current_Task;
 unsigned botton;
@@ -29,7 +40,9 @@ volatile uint8_t *currTCB;
 void savecontext() __attribute__((naked));
 void restorecontext() __attribute__((naked));
 ISR(TIMER1_COMPA_vect) __attribute__((naked));
-
+UIKSem* UIKSemCreate();
+void UIKSemPend();	// wait
+void SemPost();		// signal
 
 void Task_1() {
 	uint8_t cnt;
@@ -44,20 +57,16 @@ void Task_1() {
 }
 
 void Task_2() {
-
+	uint8_t cnt;
 	while (1) {
-	/*
+
 		for ( cnt=0x0f; cnt >= 0x00; cnt-- )
 		{
 			PORTB = ~cnt;
 			_delay_ms(500);
 			sei();
 		}
-	*/
-		PORTB = ~0x04;
-		_delay_ms(500);
-		PORTB = ~0x08;
-		_delay_ms(500);
+
 		
 	}
 }
@@ -97,55 +106,58 @@ void UIKIdle() {
     }
 }
 
+// Actually this is the Sem controller
 void Change_Switch() {
     // SW0 -> Set Task1 Ready
     if (botton == 0b11111110) {
 	TaskList[1]->status = 0;
+	SemPost(TaskList[1]->Sem);
     }
 
     // SW1 -> Set Task2 Ready
     if (botton == 0b11111101) {
 	TaskList[2]->status = 0;
+	SemPost(TaskList[2]->Sem);
     }
 
     // SW2 -> Set Task3 Ready
     if (botton == 0b11111011) {
 	TaskList[3]->status = 0;
+	SemPost(TaskList[3]->Sem);
     }
 
     // SW3 -> Set Task4 Ready
     if (botton == 0b11110111) {
 	TaskList[4]->status = 0;
+	//TaskList[4]->Sem->value = 0;
+	SemPost(TaskList[4]->Sem);
     }
 
     // SW4 -> Set Task1 Block
     if (botton == 0b11101111) {
 	TaskList[1]->status = 1;
+	//TaskList[1]->Sem->value = 1;
+	UIKSemPend(TaskList[1]->Sem);
     }
 
     // SW5 -> Set Task2 Block
     if (botton == 0b11011111) {
 	TaskList[2]->status = 1;
+	UIKSemPend(TaskList[2]->Sem);
     }
 
     // SW6 -> Set Task3 Block
     if (botton == 0b10111111) {
         TaskList[3]->status = 1;
+	UIKSemPend(TaskList[3]->Sem);
     }
 
     // SW7 -> Set Task4 Block
     if (botton == 0b01111111) {
 	TaskList[4]->status = 1;
+	UIKSemPend(TaskList[4]->Sem);
     }
 }
 
-/*
-ISR(TIMER0_OVF_vect) {
-
-    Check_Switch();
-    Change_Switch();
-	
-} // end ISR
-*/
 
 #endif
